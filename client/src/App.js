f8dc1d4d311d9910660548177b835e54d4cad186
rameshlakmal <rameshslakmal1999@gmail.com>
2022-12-06 02:40:52 +0530

    Enhance User Interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import './App.css';
 
 export default class App extends Component{
   constructor(props){
@@ -27,14 +28,39 @@ retrievePosts(){
 
   render(){
     return(
-      <div>
-        {this.state.posts.map(posts=>(
-          <div>
-            <p>{posts.topic}</p>
-            <p>{posts.description}</p>
-            <p>{posts.postCategory}</p>
-          </div>
-        ))}
+      <div className='container'>
+        <h2 className='text-center'>All Posts</h2>
+        <table className='table table-hover mt-5'>
+          <thead>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>Topic</th>
+              <th scope='col'>Description</th>
+              <th scope='col'>Post Category</th>
+              <th scope='col'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((posts,index)=>(
+              <tr>
+                <th scope="row">{index+1}</th>
+                <td>{posts.topic}</td>
+                <td>{posts.description}</td>
+                <td>{posts.postCategory}</td>
+                <td>
+                  <a className='btn btn-warning' href="#">
+                    <i className='fas fa-edit'></i>&nbsp;Edit
+                  </a>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a className='btn btn-danger' href="#">
+                  <i class="fa-regular fa-trash-can"></i>&nbsp;Delete
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    
       </div>
     )
   }
