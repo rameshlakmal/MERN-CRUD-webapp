df16bf2a13f0b7034b4ff0aefa168152073a079c
rameshlakmal <rameshslakmal1999@gmail.com>
2022-12-05 23:03:20 +0530

    Create Database Modeles

---
+++ models/posts.js
@@ -0,0 +1,20 @@
+const mongoose = require('module');
+
+const postSchema = new mongoose,Schema({
+    topic:{
+        type:String,
+        required:true
+    },
+
+    description:{
+        type:String,
+        required:true
+    },
+
+    postCategory:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('Posts',postSchema);
