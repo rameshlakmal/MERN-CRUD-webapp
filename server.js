cf3e091452e4ab406b6678d9065381937d329dc8
rameshlakmal <rameshslakmal1999@gmail.com>
2022-12-05 22:55:54 +0530

    Setup MongoDB Configuration

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 8000;
-app.listen(port, ()=>{
-    console.log(`***APP IS RUNNING ON THE SERVER***`)
+const PORT = 8000;
+
+const DB_URL = 'mongodb+srv://ramesh:mangotree1999@crud.izru9jn.mongodb.net/MONGO_DB?retryWrites=true&w=majority'
+
+
+mongoose.connect(DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+
+.then(()=>{
+    console.log('Database Connection Successful...!');
+})
+.catch((err)=>console.log('Database Connection Unsuccessful',err));
+
+
+app.listen(PORT, ()=>{
+    console.log(`***  APP IS RUNNING ON THE SERVER | PORT NO :- ${PORT}  ***`)
+});
